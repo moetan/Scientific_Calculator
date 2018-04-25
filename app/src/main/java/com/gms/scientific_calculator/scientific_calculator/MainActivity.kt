a709edf5924c60189c57f376e0cc1001a9604cf3
moetan <keikunmunini@gmail.com>
2018-04-25 10:23:49 -0700

    menu

--- app/src/main/java/com/gms/scientific_calculator/scientific_calculator/MainActivity.kt
+++ app/src/main/java/com/gms/scientific_calculator/scientific_calculator/MainActivity.kt
@@ -1,20 +1,82 @@
-package com.gms.scientific_calculator.scientific_calculator
+package test.helps.ryotakei.nav_test
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Fragment
 import android.os.Bundle
-import android.widget.Button
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
     }
-    fun setOnclicks(){
-        findViewById<Button>(R.id.calculate_button).setOnClickListener(/**something about fragments*/)
-        findViewById<Button>(R.id.graph_button).setOnClickListener(/**something about fragments*/)
-        findViewById<Button>(R.id.function_button).setOnClickListener(/**something about fragments*/)
-        findViewById<Button>(R.id.tools_button).setOnClickListener(/**something about fragments*/)
-        findViewById<Button>(R.id.games_button).setOnClickListener(/**something about fragments*/)
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_first -> {
+                println("clicked ${item.itemId}")
+                println("nav_first")
+                fragmentManager.beginTransaction().replace(R.id.contentFrame, CalcFragment()).commit()
+            }
+            R.id.nav_second -> {
+                println("clicked ${item.itemId}")
+                println("nav_second")
+                fragmentManager.beginTransaction().replace(R.id.contentFrame, Second()).commit()
+            }
+            R.id.nav_third -> {
+                println("clicked ${item.itemId}")
+                println("nav_third")
+                fragmentManager.beginTransaction().replace(R.id.contentFrame, Third()).commit()
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
---
+++ app/src/main/java/com/gms/scientific_calculator/scientific_calculator/first.kt
@@ -0,0 +1,2 @@
+package com.gms.scientific_calculator.scientific_calculator
+
