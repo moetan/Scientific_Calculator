862c3f8286a8f4b185d3183553796e8d1f5825b9
Moe <keikunmunini@gmail.com>
2018-05-07 16:00:24 -0700

    4errors

--- app/src/main/java/com/gms/scientific_calculator/scientific_calculator/MainActivity.kt
+++ app/src/main/java/com/gms/scientific_calculator/scientific_calculator/MainActivity.kt
@@ -14,6 +14,7 @@ import test.helps.ryotakei.nav_test.R
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
